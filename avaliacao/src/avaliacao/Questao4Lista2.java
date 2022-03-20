f33360b86e77c59d29ca1f575a68cf4b3aa6d4f5
Fábio Braz <fabiobrazferreira@gmail.com>
2022-03-20 18:35:05 -0300

    Atividades da disciplina Programação Orientada a Objeto!

--- avaliacao/src/avaliacao/Questao4Lista2.java
+++ avaliacao/src/avaliacao/Questao4Lista2.java
@@ -1,21 +1,28 @@
 package avaliacao;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class Questao4Lista2 {
 	public static void main(String[] args) {
 		Random random = new Random();
-		int array[] = new int[10];
+		int array[] = new int[100];
 		for(int i = 0; i < array.length; i++) {
-			array[i] = random.nextInt(10);
+			array[i] = random.nextInt(100);
 		}
 		for(int i = 0; i < array.length; i++) {
 			System.out.println("Array normal: " + array[i]);
 		}
-		System.out.println("INVERSÃO!");
-		for(int i = array.length-1; i >= 0 ; i--) {
-			System.out.println("Array invertido: " + array[i]);
+		System.out.println("Números primos!");
+		for(int i = 1; i < array.length; i++) {
+			int cont = 0;
+			for(int n = 1; n <= i; n++) {
+				if( i % n == 0) {
+					cont ++;
+				}
+			}
+		if(cont == 2) {
+			System.out.println("primo:" + i);
+		}
+			}
 		}
 	}
-}
