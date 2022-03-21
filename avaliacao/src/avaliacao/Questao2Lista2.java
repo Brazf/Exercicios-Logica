eb7132a142d6586ead376107b719da6c47b122fc
Fábio Braz <fabiobrazferreira@gmail.com>
2022-03-21 16:39:27 -0300

    Atividades da disciplina Programação Orientada a Objeto!

--- avaliacao/src/avaliacao/Questao2Lista2.java
+++ avaliacao/src/avaliacao/Questao2Lista2.java
@@ -1,13 +1,16 @@
 package avaliacao;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Questao2Lista2 {
 	public static void main(String[] args) {
 		Random random = new Random();
+		Scanner scan = new Scanner(System.in);
 		int array[] = new int[10];
 		for(int i = 0; i < array.length; i++) {
-			array[i] = random.nextInt(10);
+			System.out.print("Digite 10 números em sequência e receba-os de forma inversa, número[" + (i+1) + "] :");
+			array[i] = scan.nextInt();
 		}
 		for(int i = 0; i < array.length; i++) {
 			System.out.println("Array normal: " + array[i]);
